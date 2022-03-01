c0bdccabe9554602e0c3ccf322f282c549fb1a9e
Junior Egger <junior.egger@gmail.com>
2022-03-01 14:55:48 -0300

    Passa a funcionar por volta de 80% do cálculo do salário, faltando apenas adicionar a porcentagem.

--- scripts/businessRules.js
+++ scripts/businessRules.js
@@ -17,7 +17,36 @@ function dateLimiter() {
     document.getElementById('birthDate').setAttribute("max", data);
 }
 
+function arredondar(numero) {
+    return (Math.round(numero * 100) / 100).toFixed(2);
+}
+
 //DEFININDO SALÁRIOS
 function calculateSalary() {
     cargo = document.getElementById('occupation').value;
+    salario = document.getElementById('salary').value;
+
+    if (salario <= 0 || salario == undefined || salario == null) {
+        if (cargo == 'gerente') {
+            document.getElementById('totalSalary').value = arredondar(21000.00);
+        } else if (cargo == 'analistaDeSistemas') {
+            document.getElementById('totalSalary').value = arredondar(11000.00);
+        } else if (cargo == 'desenvolvedor') {
+            document.getElementById('totalSalary').value = arredondar(9500.00);
+        } else if (cargo == 'dba') {
+            document.getElementById('totalSalary').value = arredondar(10500.00);
+        }
+    } else {
+        // var salarioReal;
+        // if (cargo == 'gerente') {
+        //     salarioReal = salario + salario * 0.10;
+        //     document.getElementById('totalSalary').value = arredondar(salarioReal);
+        // } else if (cargo == 'analistaDeSistemas') {
+        //     salarioReal = salario;
+        //     document.getElementById('totalSalary').value = arredondar(salarioReal);
+        // } else {
+        //     document.getElementById('totalSalary').value = arredondar(salario);
+        // }
+    }
+
 }

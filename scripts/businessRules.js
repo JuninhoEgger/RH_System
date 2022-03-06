98df2c368776dd016f58bfcf366e5758e16e8bd3
Junior Egger <junior.egger@gmail.com>
2022-03-06 12:58:40 -0300

    A função dateLimiter passa a ter seu próprio arquivo.

--- scripts/businessRules.js
+++ scripts/businessRules.js
@@ -1,22 +1,3 @@
-//LIMITA A DATA PARA A DATA DE HOJE
-function dateLimiter() {
-    dataAtual = new Date();
-    dia = dataAtual.getDate();
-    mes = dataAtual.getMonth() + 1;
-    ano = dataAtual.getFullYear();
-
-    if (dia < 10) {
-        dia = '0' + dia;
-    }
-
-    if (mes < 10) {
-        mes = '0' + mes;
-    }
-
-    data = ano + "-" + mes + "-" + dia;
-    document.getElementById('birthDate').setAttribute("max", data);
-}
-
 function arredondar(numero) {
     return (Math.round(numero * 100) / 100).toFixed(2);
 }
